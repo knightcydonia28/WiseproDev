39b11537c98928e6f57e4ded9d51650a77951294
shalon14 <113790382+shalon14@users.noreply.github.com>
2022-09-18 01:02:55 -0400

    Update usermenu.php

--- backend/usermenu.php
+++ backend/usermenu.php
@@ -62,6 +62,11 @@ if(isset($_POST['but_logout'])){
         text-align: center;
 
     }
+    #body{
+        text-align: center;
+        font-size: 30px;
+        font-weight: bold;
+    }
     #icon{
         font-size: 30px;
         cursor: pointer;
@@ -77,18 +82,18 @@ if(isset($_POST['but_logout'])){
         z-index: 1;
         top: 0;
         left: 0;
-        background-color: #111;
+        background-color: #10469a;
         overflow-x: hidden;
         transition: 0.5s;
-        padding-top: 60px;
+        padding-top: 50px;
     }
     .sidenav a {
-        padding: 8px 8px 8px 32px;
+        padding: 8px 8px 10px 32px;
         text-decoration: none;
         font-size: 25px;
-        color: #818181;
+        color: black;
         display: block;
-        transition: 0.3s;
+        transition: 0.5s;
     }
 
     .sidenav a:hover {
@@ -102,7 +107,17 @@ if(isset($_POST['but_logout'])){
         font-size: 36px;
         margin-left: 50px;
     }
-
+    .logout-button{
+        margin-top:20px;
+        position: absolute;
+        right: 25px;
+        font-size: 25px;
+        margin-left: 50px;
+        border: none;
+        background-color: none;
+        cursor: pointer;
+        color: #81818;
+    }
 </style>
 </head>
 <body>
@@ -111,16 +126,18 @@ if(isset($_POST['but_logout'])){
     
     <div id="mySidenav" class="sidenav">
         <a href="javascript:void(0)" class="closenavbtn" onclick="closesideNav()">&times;</a>
+        <a href="admin.php">Home</a>
         <a href="usermenu.php">User Menu</a>
         <a href="timesheetmenu.php">Timesheet Menu</a>
         <a href="#">Create New Job</a>
         <a href="#">Timesheet Edit</a>
         <a href="#">Document Repository</a>
+        <a href="#">
+            <button class="logout-button">
+                Log out
+            </button>
+        </a>
     </div>
-
-    <form method='post' action="">
-        <input type="submit" value="Logout" name="but_logout">
-    </form>
 
     <div class='menu_area'>
         <button class="collapsible">Search User</button>
