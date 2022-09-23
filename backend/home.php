b28176e7e4214bd0cb62ac7d72099b2a1df387a6
bgebelek <113651363+bgebelek@users.noreply.github.com>
2022-09-23 16:24:47 -0400

    Session Timeout and improvements.

--- backend/home.php
+++ backend/home.php
@@ -2,26 +2,55 @@
     session_start();
     if (!isset($_SESSION['login_status'])) {
         header('Location: login.php');
+        exit();
     }
     if ($_SESSION['password_expiration'] == 0) {
         header('Location: change_password.php');
+        exit();
     }
+    if ($_SESSION['secret_key'] == 0) {
+        header('Location: setup_mfa.php');
+        exit();
+    }
+    unset($_SESSION['search_user']);
 ?>
 <!DOCTYPE html>
 <html lang="en">
     <head>
+        <?php
+            if (time() - $_SESSION['login_time'] > 900) {
+                function destroySession() {
+                    $_SESSION = array();
+                    if (ini_get("session.use_cookies")) {
+                        $params = session_get_cookie_params();
+                        setcookie(session_name(), '', time() - 42000,
+                            $params["path"], $params["domain"],
+                            $params["secure"], $params["httponly"]
+                        );
+                    }
+                    session_destroy();
+                }
+                destroySession();
+                echo 
+                "<script>
+                    alert(\"Your session has expired.\");
+                    window.location.replace(\"http://wisepro.com/testing6/login.php\");
+                </script>";
+            }
+        ?>
         <meta charset="UTF-8" />
         <title>Home</title>
     </head>
     <body>
         <?php
-            unset($_SESSION['search_user']);
-            unset($_SESSION['edit_user_authentication']);
             if (isset($_POST['logout'])) {
                 include("logout.php");
                 logout();
             }
         ?>
+        <form method="post" action="#">
+            <input type="submit" name="logout" value="Logout" />
+        </form>
         <h4>Home Page</h4>
         <ul>
             <?php
@@ -45,8 +74,5 @@
                 }
             ?>
         </ul>
-        <form method="post" action="#">
-        <input type="submit" name="logout" value="Logout" />
-        </form>
     </body>
 </html>    
