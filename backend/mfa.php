b77d1f0637c5dbb09f308ebdf40d1279bc99479d
bgebelek <113651363+bgebelek@users.noreply.github.com>
2022-09-23 16:13:44 -0400

    Session Timeout for MFA page and functionality improvements.

--- backend/mfa.php
+++ backend/mfa.php
@@ -2,18 +2,35 @@
     session_start();
     if (!isset($_SESSION['login'])) {
         header('Location: login.php');
+        exit();
     }
 ?>
 <!DOCTYPE html>
 <html lang="en">
     <head>
+        <?php
+            if (time() - $_SESSION['mfa_time'] > 60) {
+                function destroySession() {
+                    $_SESSION = array();
+                    if (ini_get("session.use_cookies")) {
+                        $params = session_get_cookie_params();
+                        setcookie(session_name(), '', time() - 42000,
+                            $params["path"], $params["domain"],
+                            $params["secure"], $params["httponly"]
+                        );
+                    }
+                    session_destroy();
+                }
+                destroySession();
+                echo 
+                "<script>
+                    alert(\"Your session has expired.\");
+                    window.location.replace(\"http://wisepro.com/testing6/login.php\");
+                </script>";
+            }
+        ?>
         <meta charset="UTF-8" />
         <title>MFA</title>
-        <script>
-            function destroySession() {
-                
-            }
-        </script>
         <style>
             .mfa_form {
                 border-style: double;
@@ -25,16 +42,28 @@
             }
         </style>
     </head>
-    <body onload="destroySession()">
+    <body>
         <br />
         <div class="mfa_form">
             <h2>Multi-factor Authentication</h2>
-            <p>Please enter your generated code below. If the code is invalid, you will be redirected to the login page.</p>
+            <p>Please enter your generated code below.</p>
             <?php
                 if (isset($_POST['mfa_code_submit'])) {
                     if (!is_numeric($_POST['mfa_code']) || strlen($_POST['mfa_code']) != 6) {
-                        include('logout.php');
-                        logout();
+                        function destroySession() {
+                            $_SESSION = array();
+                            if (ini_get("session.use_cookies")) {
+                                $params = session_get_cookie_params();
+                                setcookie(session_name(), '', time() - 42000,
+                                    $params["path"], $params["domain"],
+                                    $params["secure"], $params["httponly"]
+                                );
+                            }
+                            session_destroy();
+                        }
+                        destroySession();
+                        echo "<p>Invalid code. Please refresh this page or return to the <a href=\"login.php\">login page</a> to continue.</p>";
+                        $_SESSION['disable_mfa'] = 1;
                     }
                     else {
                         $mfa_code = $_POST['mfa_code'];
@@ -58,12 +87,26 @@
                         if ($valid) {
                             session_regenerate_id();
                             $_SESSION['login_status'] = 1;
+                            $_SESSION['login_time'] = time();
                             unset($_SESSION['login']);
                             header('Location: home.php');
+                            exit();
                         }
                         else {
-                            include('logout.php');
-                            logout();
+                            function destroySession() {
+                                $_SESSION = array();
+                                if (ini_get("session.use_cookies")) {
+                                    $params = session_get_cookie_params();
+                                    setcookie(session_name(), '', time() - 42000,
+                                        $params["path"], $params["domain"],
+                                        $params["secure"], $params["httponly"]
+                                    );
+                                }
+                                session_destroy();
+                            }
+                            destroySession();
+                            echo "<p>Invalid code. Please refresh this page or return to the <a href=\"login.php\">login page</a> to continue.</p>";
+                            $_SESSION['disable_mfa'] = 1;
                         }
                     }
                     $stmt->close();
@@ -71,8 +114,8 @@
                 }
             ?>
             <form method="post" action="#">
-                <input type="number" name="mfa_code" placeholder="000111" min="000000" max="999999" required /><br /><br />
-                <input type="submit" name="mfa_code_submit" value="Submit MFA Code" />
+                <input type="number" name="mfa_code" placeholder="000111" min="000000" max="999999" required <?php if(isset($_SESSION['disable_mfa'])) {echo "disabled";} ?> /><br /><br />
+                <input type="submit" name="mfa_code_submit" value="Submit MFA Code" <?php if(isset($_SESSION['disable_mfa'])) {echo "disabled";} ?> />
             </form>
         </div>
     </body>
