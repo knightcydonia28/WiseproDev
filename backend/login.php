bae5fcfaf24c54068b622ad169ba0fa543e3a02d
bgebelek <113651363+bgebelek@users.noreply.github.com>
2022-10-03 21:56:40 -0400

    Update login.php

--- backend/login.php
+++ backend/login.php
@@ -39,72 +39,91 @@
         <p>Please login to continue</p>
         <?php
             if (isset($_POST['login'])) {
+                
+                function test_input($data) {
+                    $data = trim($data);
+                    $data = stripslashes($data);
+                    $data = htmlspecialchars($data);
+                    return $data;
+                } 
+                
                 if (!ctype_alnum($_POST['username'])) {
                     echo "<p>Invalid username or password.</p>";
                 }
                 else {
-                    $username = $_POST['username'];
-                    $password = $_POST['password'];
-                    
-                    include("database.php");
-                    $stmt = $DBConnect->prepare("SELECT username, password FROM users WHERE username = ?");
-                    $stmt->bind_param("s", $username); 
-                    $stmt->execute();
-                    $stmt->store_result();
-                    $stmt->bind_result($retrieved_username, $retrieved_password);
-                    
-                    if ($stmt->num_rows > 0) {
-                        $stmt->fetch();
-                        if (password_verify($password, $retrieved_password)) {
-                                                        
+                        $username = test_input($_POST['username']);
+                        $password = $_POST['password'];
+                        include("database.php");
+                        $stmt = $DBConnect->prepare("SELECT username, password FROM users WHERE username = ?");
+                        $stmt->bind_param("s", $username); 
+                        $stmt->execute();
+                        $stmt->store_result();
+                        $stmt->bind_result($retrieved_username, $retrieved_password);
+                        
+                        if ($stmt->num_rows > 0) {
+                            $stmt->fetch();
+                            if (password_verify($password, $retrieved_password)) {
+                                                            
                             include("database.php");
-                            $stmt = $DBConnect->prepare("SELECT password_expiration, secret_key, user_role FROM users WHERE username = ?");
-                            $stmt->bind_param("s", $retrieved_username); 
+                            $stmt = $DBConnect->prepare("SELECT user_status FROM users WHERE username = ?");
+                            $stmt->bind_param("s", $retrieved_username);
                             $stmt->execute();
                             $stmt->store_result();
-                            $stmt->bind_result($retrieved_password_expiration, $retrieved_secret_key, $retrieved_user_role);
+                            $stmt->bind_result($retrieved_user_status);
                             $stmt->fetch();
+                            
+                            if ($retrieved_user_status == "inactive") {
+                                echo "<p>Invalid username or password.</p>";
+                            }
+                            else {
+                                $stmt = $DBConnect->prepare("SELECT password_expiration, secret_key, user_role FROM users WHERE username = ?");
+                                $stmt->bind_param("s", $retrieved_username); 
+                                $stmt->execute();
+                                $stmt->store_result();
+                                $stmt->bind_result($retrieved_password_expiration, $retrieved_secret_key, $retrieved_user_role);
+                                $stmt->fetch();
 
-                            if ($retrieved_secret_key == NULL) {
-                                session_start();
-                                $_SESSION['secret_key'] = 0;
-                            }
-                            else {
-                                session_start();
-                                $_SESSION['secret_key'] = 1;
-                            }
-                                                        
-                            if ($retrieved_password_expiration == 0) {
-                                session_regenerate_id();
-                                $_SESSION['login_status'] = 1;
-                                $_SESSION['login_time'] = time();
-                                $_SESSION['username'] = $retrieved_username;
-                                $_SESSION['password_expiration'] = $retrieved_password_expiration;
-                                $_SESSION['user_role'] = $retrieved_user_role;
-                                header('Location: change_password.php');
-                                exit();
-                            }
-                            else {
-                                if ($_SESSION['secret_key'] == 0) {
+                                if ($retrieved_secret_key == NULL) {
+                                    session_start();
+                                    $_SESSION['secret_key'] = 0;
+                                }
+                                else {
+                                    session_start();
+                                    $_SESSION['secret_key'] = 1;
+                                }
+
+                                if ($retrieved_password_expiration == 0) {
                                     session_regenerate_id();
                                     $_SESSION['login_status'] = 1;
                                     $_SESSION['login_time'] = time();
                                     $_SESSION['username'] = $retrieved_username;
                                     $_SESSION['password_expiration'] = $retrieved_password_expiration;
                                     $_SESSION['user_role'] = $retrieved_user_role;
-                                    header('Location: setup_mfa.php');
+                                    header('Location: change_password.php');
                                     exit();
                                 }
                                 else {
-                                    session_regenerate_id();
-                                    $_SESSION['mfa_time'] = time();
-                                    $_SESSION['username'] = $retrieved_username;
-                                    $_SESSION['password_expiration'] = $retrieved_password_expiration;
-                                    $_SESSION['user_role'] = $retrieved_user_role;
-                                    $_SESSION['login'] = 1;
-                                    header('Location: mfa.php');
-                                    exit();
-                                }  
+                                    if ($_SESSION['secret_key'] == 0) {
+                                        session_regenerate_id();
+                                        $_SESSION['login_status'] = 1;
+                                        $_SESSION['login_time'] = time();
+                                        $_SESSION['username'] = $retrieved_username;
+                                        $_SESSION['password_expiration'] = $retrieved_password_expiration;
+                                        $_SESSION['user_role'] = $retrieved_user_role;
+                                        header('Location: setup_mfa.php');
+                                        exit();
+                                    }
+                                    else {
+                                        session_regenerate_id();
+                                        $_SESSION['mfa_time'] = time();
+                                        $_SESSION['username'] = $retrieved_username;
+                                        $_SESSION['password_expiration'] = $retrieved_password_expiration;
+                                        $_SESSION['user_role'] = $retrieved_user_role;
+                                        $_SESSION['login'] = 1;
+                                        header('Location: mfa.php');
+                                        exit();
+                                    }  
+                                }
                             }
                         }
                         else {
@@ -114,13 +133,13 @@
                     else {
                         echo "<p>Invalid username or password.</p>";
                     }
-                    $stmt->close();
-                    $DBConnect->close();
+                $stmt->close();
+                $DBConnect->close();
                 }
             }     
         ?>
             <hr />
-            <form method="post" action="#">
+            <form method="post" action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]); ?>">
                 <input type="text" name="username" placeholder="username" required /><br /><br />
                 <input type="password" id="password" name="password" placeholder="password" required /><br /><br />
                 <input type="checkbox" onclick="passwordVisibility('password')" />Show Password <br /><br />
