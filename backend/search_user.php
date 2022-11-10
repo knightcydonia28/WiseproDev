de3da333dc26b14087ff68fb9227def45f8aec2d
bgebelek <113651363+bgebelek@users.noreply.github.com>
2022-11-10 12:19:56 -0500

    PRG Update

--- backend/search_user.php
+++ backend/search_user.php
@@ -82,7 +82,8 @@
         <h2>Search User</h2>
         <p>Please fill in one, some, or all of the following:</p>
         <?php
-            if (isset($_POST['search_user_submit'])) {
+            $search_user_error = array();
+            if ($_SERVER['REQUEST_METHOD'] === "POST") {
                 function test_input($data) {
                     $data = trim($data);
                     $data = stripslashes($data);
@@ -91,12 +92,15 @@
                 }
 
                 if (empty($_POST['username']) && empty($_POST['user_first_name']) && empty($_POST['user_middle_name']) && empty($_POST['user_last_name'])) {
-                    echo "<p class=\"error\">Please fill in at least one of the input fields.</p>";
+                    $search_user_error[] = "<p class=\"error\">Please fill in at least one of the input fields.</p>";
+                    $_SESSION["search_user_error"] = $search_user_error;
                 }
                 else {
                     if (!empty($_POST['username'])) {
                         if (!ctype_alnum($_POST['username'])) {
-                            $username_error = "Please ensure that your username is alphanumeric";
+                            $_SESSION["username_error"] = "Please ensure that your username is alphanumeric";
+                            header("Location: search_user.php", true, 303);
+                            exit();
                         }
                         else {
                             $username = test_input($_POST['username']);
@@ -107,7 +111,9 @@
                     }
                     if (!empty($_POST['user_first_name'])) {
                         if (!preg_match("/^[a-zA-Z-' ]*$/", $_POST['user_first_name'])) {
-                            $user_first_name_error = "Please ensure that your first name has letters, dashes, apostrophes and whitespaces only";
+                            $_SESSION["user_first_name_error"] = "Please ensure that your first name has letters, dashes, apostrophes and whitespaces only";
+                            header("Location: search_user.php", true, 303);
+                            exit();
                         }
                         else {
                             $user_first_name = test_input($_POST['user_first_name']);
@@ -118,7 +124,9 @@
                     }
                     if (!empty($_POST['user_middle_name'])) {
                         if (!preg_match("/^[a-zA-Z-' ]*$/", $_POST['user_middle_name'])) {
-                            $user_middle_name_error = "Please ensure that your middle name has letters, dashes, apostrophes and whitespaces only";
+                            $_SESSION["user_middle_name_error"] = "Please ensure that your middle name has letters, dashes, apostrophes and whitespaces only";
+                            header("Location: search_user.php", true, 303);
+                            exit();
                         }
                         else {
                             $user_middle_name = test_input($_POST['user_middle_name']);
@@ -129,7 +137,9 @@
                     }
                     if (!empty($_POST['user_last_name'])) {
                         if (!preg_match("/^[a-zA-Z-' ]*$/",$_POST['user_last_name'])) {
-                            $user_last_name_error = "Please ensure that your last name has letters, dashes, apostrophes and whitespaces only";
+                            $_SESSION["user_last_name_error"] = "Please ensure that your last name has letters, dashes, apostrophes and whitespaces only";
+                            header("Location: search_user.php", true, 303);
+                            exit();
                         }
                         else {
                             $user_last_name = test_input($_POST['user_last_name']);
@@ -140,20 +150,23 @@
                     }
                 }
             }
+            elseif ($_SERVER['REQUEST_METHOD'] === "GET") {
+                if (isset($_SESSION["search_user_error"])) {echo $_SESSION["search_user_error"][0];}
+            }
         ?>
         <form method="post" action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]); ?>">
             <label for="username">Username:</label>
-            <input type="text" id="username" name="username" placeholder="username" pattern="[a-zA-Z0-9]+" title="Please ensure that your username is alphanumeric"><span class="error"> <?php echo $username_error; ?></span><br><br>
+            <input type="text" id="username" name="username" placeholder="username" pattern="[a-zA-Z0-9]+" title="Please ensure that your username is alphanumeric"><span class="error"> <?php if (isset($_SESSION["username_error"])) {echo $_SESSION["username_error"];} ?></span><br><br>
             <label for="user_first_name">First Name:</label>
-            <input type="text" id="user_first_name" name="user_first_name" placeholder="first name" pattern="[a-zA-Z-'\s]*$" title="Please ensure that your first name has letters, dashes, apostrophes and whitespaces only"><span class="error"> <?php echo $user_first_name_error; ?></span><br><br>
+            <input type="text" id="user_first_name" name="user_first_name" placeholder="first name" pattern="[a-zA-Z-'\s]*$" title="Please ensure that your first name has letters, dashes, apostrophes and whitespaces only"><span class="error"> <?php if (isset($_SESSION["user_first_name_error"])) {echo $_SESSION["user_first_name_error"];} ?></span><br><br>
             <label for="user_middle_name">Middle Name:</label>
-            <input type="text" id="user_middle_name" name="user_middle_name" placeholder="middle name" pattern="[a-zA-Z-'\s]*$" title="Please ensure that your middle name has letters, dashes, apostrophes and whitespaces only"><span class="error"> <?php echo $user_middle_name_error; ?></span><br><br>
+            <input type="text" id="user_middle_name" name="user_middle_name" placeholder="middle name" pattern="[a-zA-Z-'\s]*$" title="Please ensure that your middle name has letters, dashes, apostrophes and whitespaces only"><span class="error"> <?php if (isset($_SESSION["user_middle_name_error"])) {echo $_SESSION["user_middle_name_error"];} ?></span><br><br>
             <label for="user_last_name">Last Name:</label>
-            <input type="text" id="user_last_name" name="user_last_name" placeholder="last name" pattern="[a-zA-Z-'\s]*$" title="Please ensure that your last name has letters, dashes, apostrophes and whitespaces only"><span class="error"> <?php echo $user_last_name_error; ?></span><br><br>
+            <input type="text" id="user_last_name" name="user_last_name" placeholder="last name" pattern="[a-zA-Z-'\s]*$" title="Please ensure that your last name has letters, dashes, apostrophes and whitespaces only"><span class="error"> <?php if (isset($_SESSION["user_last_name_error"])) {echo $_SESSION["user_last_name_error"];} ?></span><br><br>
             <input type="submit" name="search_user_submit" value="Search User">
         </form> 
         <?php                        
-            if (isset($_POST['search_user_submit'])) {                
+            if ($_SERVER['REQUEST_METHOD'] === "POST") {              
                 include("database.php");
                 $stmt = $DBConnect->prepare("SELECT username, user_first_name, user_middle_name, user_last_name FROM users WHERE username LIKE CONCAT(?, '%') OR user_first_name LIKE CONCAT(?, '%') OR user_middle_name LIKE CONCAT(?, '%') OR user_last_name LIKE CONCAT(?, '%')");
                 $stmt->bind_param("ssss", $username, $user_first_name, $user_middle_name, $user_last_name); 
@@ -196,12 +209,24 @@
                     echo "</table>";
                 }
                 else {
-                    echo "<p>User(s) not found.</p>";
+                    $search_user_error[] = "<p>User(s) not found.</p>";
+                    $_SESSION["search_user_error"] = $search_user_error;
+                    header("Location: search_user.php", true, 303);
+                    exit();
                 }
                 $stmt->close();
                 $DBConnect->close();
             }
+            elseif ($_SERVER['REQUEST_METHOD'] === "GET") {
+                if (isset($_SESSION["search_user_error"])) {echo $_SESSION["search_user_error"][1];}
+            }
         ?>
     </body>
 </html>
-
+<?php
+    if (isset($_SESSION["search_user_error"])) {unset($_SESSION["search_user_error"]);}
+    if (isset($_SESSION["username_error"])) {unset($_SESSION["username_error"]);}
+    if (isset($_SESSION["user_first_name_error"])) {unset($_SESSION["user_first_name_error"]);}
+    if (isset($_SESSION["user_middle_name_error"])) {unset($_SESSION["user_middle_name_error"]);}
+    if (isset($_SESSION["user_last_name_error"])) {unset($_SESSION["user_last_name_error"]);}
+?>
