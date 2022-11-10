ac9e3c2acd754e440ea05e2a8feb9346c89e4b17
bgebelek <113651363+bgebelek@users.noreply.github.com>
2022-11-10 15:19:01 -0500

    PRG Update

--- backend/edit_employment.php
+++ backend/edit_employment.php
@@ -88,18 +88,7 @@
         <p>Please fill the form below to edit the employment of the selected user:</p>
         <p><span class="error">* required field</span></p>
         <?php
-            if (!isset($_POST['edit_employment_submit'])) {
-                include("database.php");
-                $stmt = $DBConnect->prepare("SELECT clients.client_name, vendors.vendor_name, employments.job_position, employments.employment_type, employments.employment_start_date, employments.employment_status, employments.employment_end_date FROM employments INNER JOIN clients ON employments.client_id = clients.client_id LEFT OUTER JOIN vendors ON employments.vendor_id = vendors.vendor_id WHERE employments.username = ? AND employments.client_id = ?");
-                $stmt->bind_param("si", $_COOKIE['username'], $_COOKIE['client_id']);
-                $stmt->execute();
-                $stmt->store_result();
-                $stmt->bind_result($retrieved_client_name, $retrieved_vendor_name, $retrieved_job_position, $retrieved_employment_type, $retrieved_employment_start_date, $retrieved_employment_status, $retrieved_employment_end_date);
-                $stmt->fetch();
-                $stmt->close();
-                $DBConnect->close();
-            }
-            if (isset($_POST['edit_employment_submit'])) {
+            if ($_SERVER['REQUEST_METHOD'] === "POST") {
                 
                 function test_input($data) {
                     $data = trim($data);
@@ -113,22 +102,30 @@
                 }
                 
                 if (!preg_match("/^[a-zA-Z\s]*$/", $_POST['job_position'])) {
-                    $job_position_error = "Please ensure that job position has letters and whitespaces only";
+                    $_SESSION['job_position_error'] = "Please ensure that job position has letters and whitespaces only";
+                    header("Location: edit_employment_prg.php", true, 303);
+                    exit();
                 }
                 else {
                     $job_position = test_input($_POST['job_position']);
                     if ($_POST['employment_type'] != "full-time" && $_POST['employment_type'] != "part-time" && $_POST['employment_type'] != "contract" && $_POST['employment_type'] != "internship") {
-                        $employment_type_error = "Please select an appropriate employment type";
+                        $_SESSION['employment_type_error'] = "Please select an appropriate employment type";
+                        header("Location: edit_employment_prg.php", true, 303);
+                        exit();
                     }
                     else {
                         $employment_type = test_input($_POST['employment_type']);
                         $employment_start_date = test_input($_POST['employment_start_date']);
                         if (!validateDate($employment_start_date)) {
-                            $employment_start_date_error = "Please enter a valid employment start date"; 
+                            $_SESSION['employment_start_date_error'] = "Please enter a valid employment start date";
+                            header("Location: edit_employment_prg.php", true, 303);
+                            exit(); 
                         }
                         else {
                             if ($_POST['employment_status'] != "employed" && $_POST['employment_status'] != "unemployed") {
-                                $employment_status_error = "Please ensure that the employment status of the user is employed when adding employment";
+                                $_SESSION['employment_status_error'] = "Please ensure that the employment status of the user is employed when adding employment";
+                                header("Location: edit_employment_prg.php", true, 303);
+                                exit();
                             }
                             else {
                                 $employment_status = test_input($_POST['employment_status']);
@@ -138,26 +135,36 @@
                                     $stmt = $DBConnect->prepare("UPDATE employments SET job_position = ?, employment_type = ?, employment_start_date = ?, employment_status = ?, employment_end_date = ? WHERE username = ? AND client_id = ?");
                                     $stmt->bind_param("ssssssi", $job_position, $employment_type, $employment_start_date, $employment_status, $employment_end_date, $_COOKIE['username'], $_COOKIE['client_id']);
                                     if ($stmt->execute()) {
-                                        echo "<p>Changes have been made successfully.</p>"; 
+                                        $_SESSION['edit_employment_confirmation'] = "<p>Changes have been made successfully.</p>";
+                                        header("Location: edit_employment_prg.php", true, 303);
+                                        exit(); 
                                     }
                                     else {
-                                        echo "<p>Changes have not been made successfully.</p>"; 
+                                        $_SESSION['edit_employment_error'] = "<p>Changes have not been made successfully.</p>";
+                                        header("Location: edit_employment_prg.php", true, 303);
+                                        exit(); 
                                     }
                                 }
                                 if ($employment_status == "unemployed" && !empty($_POST['employment_end_date'])) {
                                     $employment_end_date = test_input($_POST['employment_end_date']);
                                     if (!validateDate($employment_end_date)) {
-                                        $employment_end_date_error = "Please enter a valid employment end date"; 
+                                        $_SESSION['employment_end_date_error'] = "Please enter a valid employment end date";
+                                        header("Location: edit_employment_prg.php", true, 303);
+                                        exit();
                                     }
                                     else {
                                         include("database.php");
                                         $stmt = $DBConnect->prepare("UPDATE employments SET job_position = ?, employment_type = ?, employment_start_date = ?, employment_status = ?, employment_end_date = ? WHERE username = ? AND client_id = ?");
                                         $stmt->bind_param("ssssssi", $job_position, $employment_type, $employment_start_date, $employment_status, $employment_end_date, $_COOKIE['username'], $_COOKIE['client_id']);
                                         if ($stmt->execute()) {
-                                            echo "<p>Changes have been made successfully.</p>"; 
+                                            $_SESSION['edit_employment_confirmation'] = "<p>Changes have been made successfully.</p>";
+                                            header("Location: edit_employment_prg.php", true, 303);
+                                            exit(); 
                                         }
                                         else {
-                                            echo "<p>Changes have not been made successfully.</p>"; 
+                                            $_SESSION['edit_employment_error'] = "<p>Changes have not been made successfully.</p>";
+                                            header("Location: edit_employment_prg.php", true, 303);
+                                            exit(); 
                                         }
                                     }
                                 }
@@ -165,23 +172,34 @@
                         }   
                     } 
                 }       
-                $stmt->close();
-                $DBConnect->close();
             }
+            elseif ($_SERVER['REQUEST_METHOD'] === "GET") {
+                if (isset($_SESSION['edit_employment_confirmation'])) {echo $_SESSION['edit_employment_confirmation'];}
+                if (isset($_SESSION['edit_employment_error'])) {echo $_SESSION['edit_employment_error'];}
+            }
+            include("database.php");
+            $stmt = $DBConnect->prepare("SELECT clients.client_name, vendors.vendor_name, employments.job_position, employments.employment_type, employments.employment_start_date, employments.employment_status, employments.employment_end_date FROM employments INNER JOIN clients ON employments.client_id = clients.client_id LEFT OUTER JOIN vendors ON employments.vendor_id = vendors.vendor_id WHERE employments.username = ? AND employments.client_id = ?");
+            $stmt->bind_param("si", $_COOKIE['username'], $_COOKIE['client_id']);
+            $stmt->execute();
+            $stmt->store_result();
+            $stmt->bind_result($retrieved_client_name, $retrieved_vendor_name, $retrieved_job_position, $retrieved_employment_type, $retrieved_employment_start_date, $retrieved_employment_status, $retrieved_employment_end_date);
+            $stmt->fetch();
+            $stmt->close();
+            $DBConnect->close();
         ?>
         <form method="post" action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]); ?>">
             <label for="username">Username:</label>
             <input type="text" id="username" name="username" placeholder="username" pattern="[a-zA-Z0-9]+" title="Please ensure that your username is alphanumeric" value="<?php if (isset($_COOKIE["username"])) {echo $_COOKIE["username"];} ?>" readonly required><br><br>
             <label for="client_name">Client:</label>
-            <input type="text" name="client_name" id="client_name" placeholder="client" pattern="^[a-zA-Z\s]*$" title="Please ensure that client name has letters and whitespaces only" value="<?php if (isset($_POST['edit_employment_submit'])) {echo $_POST['client_name'];} else {echo $retrieved_client_name;} ?>" readonly required><br><br>
+            <input type="text" name="client_name" id="client_name" placeholder="client" pattern="^[a-zA-Z\s]*$" title="Please ensure that client name has letters and whitespaces only" value="<?php echo $retrieved_client_name; ?>" readonly required><br><br>
             <label for="vendor_name">Vendor:</label>
-            <input type="text" name="vendor_name" id="vendor_name" placeholder="vendor" pattern="^[a-zA-Z\s]*$" title="Please ensure that vendor name has letters and whitespaces only" value="<?php if (isset($_POST['edit_employment_submit'])) {echo $_POST['vendor_name'];} else {echo $retrieved_vendor_name;} ?>" readonly required><br><br>
+            <input type="text" name="vendor_name" id="vendor_name" placeholder="vendor" pattern="^[a-zA-Z\s]*$" title="Please ensure that vendor name has letters and whitespaces only" value="<?php echo $retrieved_vendor_name; ?>" readonly required><br><br>
             <label for="job_position">Job Position:</label>
-            <input list="job_positions" name="job_position" id="job_position" placeholder="job position" pattern="^[a-zA-Z\s]*$" title="Please ensure that job position has letters and whitespaces only" value="<?php if (isset($_POST['edit_employment_submit'])) {echo $_POST['job_position'];} else {echo $retrieved_job_position;} ?>" required><span class="error"> * <?php echo $job_position_error; ?></span>
+            <input list="job_positions" name="job_position" id="job_position" placeholder="job position" pattern="^[a-zA-Z\s]*$" title="Please ensure that job position has letters and whitespaces only" value="<?php echo $retrieved_job_position; ?>" required><span class="error"> * <?php if (isset($_SESSION['job_position_error'])) {echo $_SESSION['job_position_error'];} ?></span>
                 <datalist id="job_positions">
                     <?php
                         include("database.php");
-                        $stmt = $DBConnect->prepare("SELECT job_position FROM employments");
+                        $stmt = $DBConnect->prepare("SELECT DISTINCT job_position FROM employments");
                         $stmt->execute();
                         $stmt->store_result();
                         $stmt->bind_result($retrieved_job_position);
@@ -195,38 +213,40 @@
             <label for="employment_type">Employment Type:</label>
             <select id="employment_type" name="employment_type" required>
                 <option value="">&nbsp;</option>
-                <option value="full-time" <?php if (isset($_POST['edit_employment_submit']) && isset($_POST['employment_type']) && $_POST['employment_type'] == "full-time") {echo "selected";} else {if ($retrieved_employment_type == "full-time") {echo "selected";}} ?>>Full-time</option>
-                <option value="part-time" <?php if (isset($_POST['edit_employment_submit']) && isset($_POST['employment_type']) && $_POST['employment_type'] == "part-time") {echo "selected";} else {if ($retrieved_employment_type == "part-time") {echo "selected";}} ?>>Part-time</option>
-                <option value="contract" <?php if (isset($_POST['edit_employment_submit']) && isset($_POST['employment_type']) && $_POST['employment_type'] == "contract") {echo "selected";}  else {if ($retrieved_employment_type == "contract") {echo "selected";}} ?>>Contract</option>
-                <option value="internship" <?php if (isset($_POST['edit_employment_submit']) && isset($_POST['employment_type']) && $_POST['employment_type'] == "internship") {echo "selected";} else {if ($retrieved_employment_type == "internship") {echo "selected";}} ?>>Internship</option>
-            </select><span class="error"> * <?php echo $employment_type_error; ?></span><br><br>
+                <option value="full-time" <?php if ($retrieved_employment_type == "full-time") {echo "selected";} ?>>Full-time</option>
+                <option value="part-time" <?php if ($retrieved_employment_type == "part-time") {echo "selected";} ?>>Part-time</option>
+                <option value="contract" <?php if ($retrieved_employment_type == "contract") {echo "selected";} ?>>Contract</option>
+                <option value="internship" <?php if ($retrieved_employment_type == "internship") {echo "selected";} ?>>Internship</option>
+            </select><span class="error"> * <?php if (isset($_SESSION['employment_type_error'])) {echo $_SESSION['employment_type_error'];} ?></span><br><br>
             <label for="employment_start_date">Employment Start Date:</label>
-            <input type="date" id="employment_start_date" name="employment_start_date" required><span class="error"> * <?php echo $employment_start_date_error; ?></span><br><br>
+            <input type="date" id="employment_start_date" name="employment_start_date" required><span class="error"> * <?php if (isset($_SESSION['employment_start_date_error'])) {echo $_SESSION['employment_start_date_error'];} ?></span><br><br>
             <label for="employment_status">Employment Status:</label>
             <select id="employment_status" name="employment_status" onchange="checkDropdown()" required>
                 <option value="">&nbsp;</option>
-                <option value="employed" <?php if (isset($_POST['edit_employment_submit']) && isset($_POST['employment_status']) && $_POST['employment_status'] == "employed") {echo "selected";} else {if ($retrieved_employment_status == "employed") {echo "selected";}} ?>>Employed</option>
-                <option value="unemployed" <?php if (isset($_POST['edit_employment_submit']) && isset($_POST['employment_status']) && $_POST['employment_status'] == "unemployed") {echo "selected";} else {if ($retrieved_employment_status == "unemployed") {echo "selected";}} ?>>Unemployed</option>
-            </select><span class="error"> * <?php echo $employment_status_error; ?></span><br><br>
+                <option value="employed" <?php if ($retrieved_employment_status == "employed") {echo "selected";} ?>>Employed</option>
+                <option value="unemployed" <?php if ($retrieved_employment_status == "unemployed") {echo "selected";} ?>>Unemployed</option>
+            </select><span class="error"> * <?php if (isset($_SESSION['employment_status_error'])) {echo $_SESSION['employment_status_error'];} ?></span><br><br>
             <label for="employment_end_date">Employment End Date:</label>
-            <input type="date" id="employment_end_date" name="employment_end_date" required><span id="employment_end_date_asterisk" class="error"> <?php echo $employment_end_date_error; ?></span><br><br>
+            <input type="date" id="employment_end_date" name="employment_end_date" required><span id="employment_end_date_asterisk" class="error"> <?php if (isset($_SESSION['employment_end_date_error'])) {echo $_SESSION['employment_end_date_error'];} ?></span><br><br>
             <input type="submit" name="edit_employment_submit" value="Submit Changes">
         </form>
         <?php
-            if (!isset($_POST['edit_employment_submit'])) {
+            if ($_SERVER['REQUEST_METHOD'] === "GET" || !isset($_POST['edit_employment_submit'])) {
                 echo
                 "<script>
-                    document.getElementById(\"employment_start_date\").value = \""; echo $retrieved_employment_start_date; echo "\";
-                    document.getElementById(\"employment_end_date\").value = \""; echo $retrieved_employment_end_date; echo "\";
-                </script>";
-            }
-            if (isset($_POST['edit_employment_submit'])) {
-                echo
-                "<script>
-                    document.getElementById(\"employment_start_date\").value = \""; echo $_POST['employment_start_date']; echo "\";
-                    document.getElementById(\"employment_end_date\").value = \""; echo $_POST['employment_end_date']; echo "\";
+                    document.getElementById(\"employment_start_date\").value = \"".$retrieved_employment_start_date."\"
+                    document.getElementById(\"employment_end_date\").value = \"".$retrieved_employment_end_date."\"
                 </script>";
             }
         ?>
     </body>
 </html>
+<?php
+    if (isset($_SESSION['job_position_error'])) {unset($_SESSION['job_position_error']);}
+    if (isset($_SESSION['employment_type_error'])) {unset($_SESSION['employment_type_error']);}
+    if (isset($_SESSION['employment_start_date_error'])) {unset($_SESSION['employment_start_date_error']);}
+    if (isset($_SESSION['employment_status_error'])) {unset($_SESSION['employment_status_error']);}
+    if (isset($_SESSION['employment_end_date_error'])) {unset($_SESSION['employment_end_date_error']);}
+    if (isset($_SESSION['edit_employment_confirmation'])) {unset($_SESSION['edit_employment_confirmation']);}
+    if (isset($_SESSION['edit_employment_error'])) {unset($_SESSION['edit_employment_error']);}
+?>
